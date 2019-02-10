d86ce438b901d8b57ed6b35fb1430f37f0db2aad
Aditya <aditya@diwakar.io>
2019-02-09 23:22:20 -0500

    Previous commit ignored history, added appending for logging
    Next Steps:
    - Write ZeroMQ microservice
    
    Notes: Deployed due to incompleteness

--- trello/update.py
+++ trello/update.py
@@ -5,8 +5,11 @@ from flask_restful import Api, Resource, reqparse
 
 class latest(Resource):
         def post(self):
-                with open("logs.txt", "w") as f:
-                        f.write(str(request.json))
+                with open("logs.json", "r") as f:
+                        data = json.load(f)
+                data.append(request.json)
+                with open("logs.json", "w") as f:
+                        json.dump(data, f, indent=4)
                 return "", 200
 
         def head(self):
